be1e428d2c568f9a611a0d0906b477efce8b253a
ImrajsItera <imrich.kubovic@itera.com>
2022-05-03 11:50:22 +0200

    First commit

--- Imrajs-cypress/cypress/Imrajs_AT/AboutYouTest.spec.js
+++ Imrajs-cypress/cypress/Imrajs_AT/AboutYouTest.spec.js
@@ -1,5 +1,30 @@
+
 describe ("AboutYou homework", () => {
-    before(()=> {
-         cy.visit('https://aboutyou.sk')
+    Cypress.on("uncaught:exception",(err,runnable)=>{
+        return false;
+       
+       
+    });
+    beforeEach(()=> {
+         cy.clearLocalStorage();
+         cy.clearCookies();
+        
+         cy.setCookie("OptanonAlertBoxCLosed", "2022-04-14T21:40:00.00Z")
+         cy.visit('https://www.aboutyou.sk/?gender=male')
+         cy.wait(2000)
+         
     })
+    it('Choose two items and add them into basket', ()=>{
+       // cy.get('[id=onetrust-accept-btn-handler]').click()
+        cy.get('[data-test-id=Label_Oblečenie]').click()
+        cy.get('[data-testid=linkInactive-20324]').click()
+        cy.wait(1500)
+        cy.get('[data-testid=productTile-4254016]').click()
+        //cy.get('[data-testid=productColorOptionsOption2]',{timeout:6000}).click()
+        cy.get('[data-testid=sizeFlyout').click()
+        cy.get('[data-testid=sizeOption_37233440_active]').click()
+        cy.get('[data-testid=addToBasketButton]').click()
+        
+        
+    }) 
 })
